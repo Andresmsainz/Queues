1017cd7c753d6c4c5d2adc674dfc41de83da740e
Andres Sainz <andresmsainz@gmail.com>
2021-05-23 21:56:37 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queues!");
+
+            //Creating
+            Console.WriteLine("In Creating");
+            Queue queue = new Queue();
+            Console.WriteLine("-----------");
+
+            //Adding
+            Console.WriteLine("In Adding");
+            queue.Enqueue("Calvin");
+            queue.Enqueue("Tom");
+            queue.Enqueue("Emma");
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine("Item = " + item);
+            }
+
+            Console.WriteLine("-----------");
+            //Removing
+            Console.WriteLine("In Removing");
+            Console.WriteLine("Removing = " + queue.Dequeue());
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine("Item = " + item);
+            }
+
+            Console.WriteLine("-----------");
+        }
+    }
+}
